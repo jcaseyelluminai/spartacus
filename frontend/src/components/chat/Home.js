a23ea410b9159b7975b872ffb68f525c6d4403d4
David Nwachukwu <davidnw1998@gmail.com>
2020-06-22 10:33:08 +0100

    add content to home page

--- frontend/src/components/chat/Home.js
+++ frontend/src/components/chat/Home.js
@@ -10,13 +10,37 @@ const Home = () => {
             Spartacus is a modular cognitive system used by the AD Cloud to
             extract deep insights, often in real-time, from asset, operational
             and external data sources. It is a key part of AD Cloud’s autonomous
-            UAV initiative. A natural language processing (NLP) digital agent
-            with deep neural learning abilities and a modular architecture,
-            Spartacus can learn specialized skills, developed on a case by case
-            basis.
+            UAV initiative.
+          </p>
+          <p>
+            A natural language processing (NLP) digital agent with deep neural
+            learning abilities and a modular architecture, Spartacus can learn
+            specialized skills, developed on a case by case basis.
           </p>
         </div>
-        <div className="content-img">More Text</div>
+        <div className="content-img">
+          <img src="/static/sparticusimg1.png" alt="image1" />
+        </div>
+      </div>
+      <div className="content">
+        <div className="content-img">
+          <img src="/static/sparticusimg2.png" alt="image1" />
+        </div>
+        <div className="content-text">
+          <h3>How it Works</h3>
+          <p>
+            Spartacus is based on neural networks, hence the ability for deep
+            learning. By employing its algorithms in a modular fashion,
+            Spartacus can learn different types of skills, providing valuable
+            insights to many industries.
+          </p>
+          <p>
+            The outcome? More valuable insights in less time from complex data
+            with ever-increasing efficiency. However, it’s important that
+            Spartacus is given the right sets of data and not ‘kept in the
+            dark’.
+          </p>
+        </div>
       </div>
     </div>
   );
